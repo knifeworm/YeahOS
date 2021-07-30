ca0e409cea8ed46028730a441c56b8f78840c53d
knifeworm <77696553+knifeworm@users.noreply.github.com>
2021-07-30 12:02:33 +0100

    Having to start again
    F

--- kernel/modules/com/LegacySerial.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <libutils/Lock.h>
-#include <libutils/RingBuffer.h>
-
--- thirdparty/.build.mk
+++
@@ -1,9 +0,0 @@
-ECHFS:=thirdparty/echfs/echfs-utils
-LIMINE:=thirdparty/limine/limine-install
-LIMINE_LOADER:=thirdparty/limine/limine.bin
-
-$(ECHFS):
-	cd thirdparty/echfs/ && $(MAKE) all
-
-$(LIMINE):
-	cd thirdparty/limine/ $$ $(MAKE) all
